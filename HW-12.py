b9a62edfa241ed4aa7829f9401ba4e36e8d72c92
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-01 16:41:08 +0300

    HW12 fixies

--- HW-12.py
+++ HW-12.py
@@ -1,3 +1,6 @@
-range_years = range(2000, 2050)
-result = [i for i in range_years if i % 4 == 0 or i % 400 == 0]
+start = int(input('Начальный год: '))
+end = int(input('Конечный год: '))
+if start >= end:
+    print('Просчет невозможен. Конечный год должен быть больше начального')
+result = [i for i in range(start, end) if i % 4 == 0 or i % 400 == 0]
 print(' '.join(map(str, result)))
