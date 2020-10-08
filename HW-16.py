5fdfb33fc0272dbe2d9a0c4921d9b108aea57b43
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-08 23:45:15 +0300

    HW16 upload

---
+++ HW-16.py
@@ -0,0 +1,18 @@
+my_dict = {'orange': None,
+           'red': 'красный',
+           'green': None,
+           'yellow': 'желтый',
+           'black': None}
+result = {k: v for k, v in my_dict.items() if v is not None}
+print(result)
+
+
+
+
+
+
+
+
+
+
+
