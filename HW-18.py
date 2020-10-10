92c8d0bcd28d4acbed95f8c59a1353b5c9addd4f
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-10 11:49:20 +0300

    HW18-fix2

--- HW-18.py
+++ HW-18.py
@@ -5,7 +5,7 @@ def shopping_in_store(goods_list) -> tuple:
         if stock[product] > 0:
             result += prices[product]
             stock[product] -= 1
-        if stock[product] <= 0:
+        if stock[product] == 0:
             print('Note! Some of your needed products are out of stock. Please come later or pre-order them.')
     return f'Price is: {result}', '\n'.join(f'{k}: {v}' for k, v in stock.items())
 
@@ -17,8 +17,8 @@ prices = {'iphone': 1500,
           'ipad': 1300,
           'appleTV': 800}
 stock = {'iphone': 15,
-         'macbook': 4,
-         'airpods': 0,
+         'macbook': 0,
+         'airpods': 3,
          'ipad': 5,
          'appleTV': 25}
 print(*(shopping_in_store(shop_list)), sep='\nStock balance: \n')
