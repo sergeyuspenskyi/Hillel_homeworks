1f8c1593b3261a7108694310985e4ca1aeb41f9f
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-17 18:49:50 +0300

    HW-21 upload

---
+++ HW-21.py
@@ -0,0 +1,26 @@
+# Task 1
+# sentence = input('Input your sentence: ')
+# print([len(i) for i in sentence.split()])
+
+# Task 2
+# list_1 = [-121.1, 222, 0, 29, -12, -1]
+# print([abs(i) for i in list_1])
+
+# Task 3
+# print([i ** 2 for i in range(1, 11) if i % 2 == 0])
+
+# Task 4
+# print([i for i in range(1, 11) if (i * i * i) % 4 == 0])
+
+# Task 5
+# first_list = [i for i in range(1, 22)]
+# second_list = [odd for odd in first_list if odd % 2 != 0]
+# middle_third = first_list[7:14]
+# print(middle_third)
+
+# Task 6
+# print([number * number for number in range(1, 11) if 30 < (number * number) < 70])
+
+# Task 7
+# mystery = '!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI'
+# print(mystery[::-2])
