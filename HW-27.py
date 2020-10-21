969df8f5af17b5906fe673be119069f3ad68edf9
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-22 00:04:38 +0300

    HW-27 fix

--- HW-27.py
+++ HW-27.py
@@ -1,15 +1,23 @@
-from operator import itemgetter
-with open('input.txt', 'r') as poem:
-    poem = poem.read().title()
-    bad_chars = ['-', ',', '.', ':']
-    poem = ''.join((filter(lambda i: i not in bad_chars, poem)))
-    poem = poem.strip().split()
-    words_dict = dict()
-    for word in poem:
-        if word in words_dict:
-            words_dict[word] = words_dict[word] + 1
+def txt_file_handler(file) -> list:
+    """Deletes 'bad' chars from txt. file except words(letters) and converts to list"""
+    with open(file, 'r') as poem:
+        bad_chars = ['-', ',', '.', ':']
+        poem = poem.read().title()
+        poem = ''.join((filter(lambda i: i not in bad_chars, poem)))
+    return poem.strip().split()
+
+
+def word_counter(list_) -> list:
+    """Counts and sorts words from a list"""
+    from operator import itemgetter
+    _dict = dict()
+    for word in list_:
+        if word in _dict:
+            _dict[word] = _dict[word] + 1
         else:
-            words_dict[word] = 1
-    final_dict = sorted(words_dict.items(), key=itemgetter(1), reverse=True)
-    print('Amount of words: ', len(poem))
-    print('Frequency: ', '\n'.join(f'{k}: {v}' for k, v in final_dict), sep='\n')
+            _dict[word] = 1
+    return sorted(_dict.items(), key=itemgetter(1), reverse=True)
+
+
+print('Amount of words: ', len(txt_file_handler('input.txt')))
+print('Frequency: ', '\n'.join(f'{k}: {v}' for k, v in word_counter(txt_file_handler('input.txt'))), sep='\n')
